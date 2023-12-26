f1521e585917443305d6591ca26d24ef8cbcfd5c
Alpha Olomi <alphaolomi@gmail.com>
2023-12-26 10:22:50 +0000

    Update page header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,13 @@ export default function IndexPage() {
       <header className="px-4 lg:px-6 h-16 flex items-center bg-red-600 text-white">
         <Link className="flex items-center justify-center" href="#">
           <TreesIcon className="h-6 w-6" />
-          <span className="sr-only">Holiday Inc</span>
+          <span className="sr-only">Alpha Olomi</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
             Home
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+          {/* <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
             Events
           </Link>
           <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
@@ -24,20 +24,22 @@ export default function IndexPage() {
           </Link>
           <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
             Contact
-          </Link>
+          </Link> */}
         </nav>
       </header>
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-red-600 text-white text-center">
           <div className="container px-4 md:px-6">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">Happy Holidays!</h1>
+            <h1 className="text-4xl md:text-6xl font-bold mb-4"> 🎄 Merry Christmas
+              <span className="text-4xl md:text-6xl font-bold mb-4"> & </span>
+              🎉 Happy New Year</h1>
             <p className="text-lg md:text-xl max-w-prose mx-auto">
-              Join us in celebrating the joyous moments of Christmas and New Year.
+              Have a wonderful Christmas and a Happy New Year! May your Christmas sparkle with moments of love, laughter and goodwill, And may the year ahead be full of contentment and joy.
             </p>
-            <Button className="mt-4 bg-white text-red-600 mx-auto">Learn More</Button>
+            <Button className="mt-4 bg-white text-red-600 mx-auto">👋 Wave hello</Button>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        {/* <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center">Upcoming Events</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -70,10 +72,10 @@ export default function IndexPage() {
               </Card>
             </div>
           </div>
-        </section>
+        </section> */}
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 bg-red-600 text-white">
-        <p className="text-xs">© Holiday Inc. All rights reserved.</p>
+        <p className="text-xs">Made with 💚 by <a className="hover:underline" href="https://alphaolomi.com">Alpha Olomi</a> </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Terms of Service
